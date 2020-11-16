3e0fb02420dfdde476965d6647af41709c8c805f
ubourdon <bourdon.ugo@gmail.com>
2020-11-16 11:09:35 +0100

    add: circe json bindings for SafeUUID

--- project/ApplicationBuildConfig.scala
+++ project/ApplicationBuildConfig.scala
@@ -6,25 +6,35 @@ import Dependencies._
 object ApplicationBuildConfig {
 	val groupId = "org.perfimmo"
 	val appName = "pi-prelude"
-	val appVersion = "0.1.2-SNAPSHOT"
+	val appVersion = "0.1.3-SNAPSHOT"
 	val versionOfScala = "2.13.3"
 
 	val appDependencies =
 		zioDependencies ++
 		javaDepencencies ++
+		circeDeps ++
 		testDependencies
 
 }
 
 object Dependencies {
 	val zio_version = "1.0.1"
+	val zio_prelude_version = "1.0.0-RC1"
+	val circeVersion = "0.12.3"
 
 	val zioDependencies = Seq(
 		"dev.zio"                           %% "zio"                                % zio_version,
 		"dev.zio"                           %% "zio-streams"                        % zio_version,
 		//"dev.zio"                         %% "zio-interop-reactivestreams"                    % "1.0.3.5",
-		"dev.zio"                           %% "zio-prelude"                        % "1.0.0-RC1"
+		"dev.zio"                           %% "zio-prelude"                        % zio_prelude_version
 	)
+
+	val circeDeps = Seq(
+		"io.circe" %% "circe-core",
+		"io.circe" %% "circe-generic",
+		"io.circe" %% "circe-parser"
+	).map(_ % circeVersion)
+
 
 	val javaDepencencies = Seq(
 	)
---
+++ src/main/scala/pi/prelude/bindings/json/circe/SafeUUIDJsonCodec.scala
@@ -0,0 +1,13 @@
+package pi.prelude.bindings.json.circe
+
+import io.circe.Decoder.Result
+import io.circe.{Codec, DecodingFailure, HCursor, Json}
+import pi.prelude.safeuuid.SafeUUID
+
+object SafeUUIDJsonCodec {
+	implicit val safeUUIDJsonCodec: Codec[SafeUUID] = new Codec[SafeUUID] {
+		override def apply(a: SafeUUID): Json = Json.fromString(a.safeValue)
+		override def apply(c: HCursor): Result[SafeUUID] =
+			c.as[String].flatMap { SafeUUID(_).leftMap { s => DecodingFailure(s, Nil) } }
+	}
+}
