8f5d7161923322ad27d430b53a11710c2319d72f
ubourdon <bourdon.ugo@gmail.com>
2023-06-19 17:32:06 +0200

    release: 0.11

--- project/ApplicationBuildConfig.scala
+++ project/ApplicationBuildConfig.scala
@@ -6,7 +6,7 @@ import Dependencies._
 object ApplicationBuildConfig {
 	val groupId = "org.perfimmo"
 	val appName = "pi-prelude"
-	val appVersion = "0.1.11-SNAPSHOT"
+	val appVersion = "0.1.11"
 	val versionOfScala = "2.13.3"
 
 	val appDependencies =
